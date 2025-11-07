b1908b9e145eeed963be6b56dcc79ad4293ba4b0
Hani20086khalil <hani20086khalil@gmail.com>
2025-11-07 13:50:07 +0200

    fix new build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,10 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-<<<<<<< Updated upstream
-    serviceWorkerVersion: "648905008"
-=======
-    serviceWorkerVersion: "2896822832"
->>>>>>> Stashed changes
+    serviceWorkerVersion: "3430062562"
   }
 });
